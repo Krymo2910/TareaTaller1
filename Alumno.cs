e104992e9d92f20979cbb076f0958ccbe2bb7a77
Pablo <krymo2910w@gmail.com>
2026-09-20 23:50:37 -0500

    test

---
+++ Alumno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea1Taller
+{
+    internal class Alumno
+    {
+        public string nombre;
+        public float nota1;
+        public float nota2;
+        public float nota3;
+    }
+}
